07aac082e6b49007d48bd663324dfac7f5167202
DImiir <d.i.mironenko@yandex.ru>
2024-03-02 18:04:11 +0300

    задача "Промежуточный этап создания приложения"

--- code.py
+++ code.py
@@ -1,29 +1,95 @@
-from flask import Flask
-from data.__all_models import User, Jobs
+from flask import Flask, redirect, render_template
+from data.__all_models import *
 from data import db_session
-import datetime
-import time
-
+from flask_login import LoginManager, login_user, current_user, login_required, logout_user
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 
-def main():
-    db_session.global_init("db/users.sqlite")
-
-    job = Jobs()
-    job.team_leader = "1"
-    job.job = "deployment of residential modules 1 and 2"
-    job.work_size = "15"
-    job.collaborators = "2, 3"
-    job.start_date = datetime.datetime.now
-    job.is_finished = False
+@login_manager.user_loader
+def load_user(user_id):
     db_sess = db_session.create_session()
-    db_sess.add(job)
-    db_sess.commit()
+    return db_sess.query(User).get(user_id)
 
-    # app.run()
+
+@app.route('/')
+@app.route('/index/<title>')
+def index(title='main'):
+    if current_user.is_authenticated:
+        db_sess = db_session.create_session()
+        jobs = db_sess.query(Jobs).filter(Jobs.team_leader.like(current_user.id) | Jobs.collaborators.contains(current_user.id))
+    else:
+        jobs = None
+    return render_template('base.html', title=title, jobs=jobs)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = User()
+        user.surname = form.surname.data
+        user.name = form.name.data
+        user.age = form.age.data
+        user.position = form.position.data
+        user.speciality = form.speciality.data
+        user.address = form.address.data
+        user.email = form.email.data
+        user.hashed_password = generate_password_hash(form.hashed_password.data)
+        user.modified_date = form.modified_date.data
+        db_sess.add(user)
+        db_sess.commit()
+        login_user(user)
+        return redirect('/')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.verify_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/add_job', methods=['GET', 'POST'])
+@login_required
+def add_job():
+    form = JobForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        jobs = Jobs()
+        jobs.team_leader = form.team_leader.data
+        jobs.job = form.job.data
+        jobs.work_size = form.work_size.data
+        jobs.collaborators = form.collaborators.data
+        jobs.start_date = form.start_date.data
+        jobs.end_date = form.end_date.data
+        jobs.is_finished = form.is_finished.data
+        db_sess.add(jobs)
+        db_sess.commit()
+        return redirect('/')
+    return render_template('jobadd.html', title='Добавление работы', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
 
 
 if __name__ == '__main__':
-    main()
+    db_session.global_init("db/users.sqlite")
+    app.run(port=8080, host='127.0.0.1')
